7aa458a5e1d8671a4b31c71c95fe9c96201b3137
root <root@DESKTOP-G988E0G>
2023-03-21 10:19:18 +0900

    최초커밋 230321

---
+++ restSeqApi/src/main/java/com/rok/seq/RestSeqApiApplication.java
@@ -0,0 +1,13 @@
+package com.rok.seq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSeqApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSeqApiApplication.class, args);
+	}
+
+}
---
+++ restSeqApi/src/test/java/com/rok/seq/RestSeqApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rok.seq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSeqApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
