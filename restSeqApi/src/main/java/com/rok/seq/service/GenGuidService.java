23219c50e22223c0c1f2fc770ee7217d16d61be2
root <root@DESKTOP-G988E0G>
2023-03-22 18:30:22 +0900

    guid 중복방지 코드 추가

--- restSeqApi/src/main/java/com/rok/seq/service/GenGuidService.java
+++ restSeqApi/src/main/java/com/rok/seq/service/GenGuidService.java
@@ -2,17 +2,25 @@ package com.rok.seq.service;
 
 import java.io.IOException;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 
 import com.rok.seq.controller.dto.GuidInDto;
+import com.rok.seq.service.dto.SequenceStateDto;
 import com.rok.seq.utils.DateUtils;
+import com.rok.seq.utils.StringUtils;
 
 @Service
 public class GenGuidService {
 
 	private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 	private static final Random RANDOM = new Random();
+	@Autowired
+	private RedisTemplate<String, String> redisTemplateString;
 
 	/**
 	 * guid 생성 요청 시스템코드와 해당 시스템의 노드번호, 인스턴스번호를 받아 guid를 채번한다. 규칙:
@@ -30,15 +38,26 @@ public class GenGuidService {
 		append(guidBuffer, String.format("%02d", in.getSendSysNodeNo()));
 		append(guidBuffer, String.format("%02d", in.getSendSysInstNo()));
 		append(guidBuffer, generateRandomString(6));
+		
+		String guid = guidBuffer.toString() ;
+		
+		ValueOperations<String, String> vop = redisTemplateString.opsForValue();
+		String value = vop.get(guid);
+		
+		if(StringUtils.isEmpty(value)) {
+			vop.set(guid, "dup", 1, TimeUnit.SECONDS) ;
+		} else {
+			throw new RuntimeException("guid 중복이 발생하였습니다. [" + guid +"]") ;
+		}
 
-		return guidBuffer.toString();
+		return guid;
 	}
 
-	private static Appendable append(Appendable appendable, CharSequence sequence) {
+	private Appendable append(Appendable appendable, CharSequence sequence) {
 		return append(appendable, sequence, sequence.length());
 	}
 
-	private static Appendable append(Appendable appendable, CharSequence sequence, int length) {
+	private Appendable append(Appendable appendable, CharSequence sequence, int length) {
 		try {
 			for (int i = 0; i < length; i++)
 				appendable.append(sequence.charAt(i));
@@ -47,7 +66,7 @@ public class GenGuidService {
 		return appendable;
 	}
 	
-	private static String generateRandomString(int length) {
+	private String generateRandomString(int length) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < length; i++) {
             int randomIndex = RANDOM.nextInt(CHARACTERS.length());
